e983dbd49afe5ef9c47aa90f37e17171a2389753
sharmaayush176031-ux <sharmaayush176031@gmail.com>
2026-04-19 13:47:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask App Running 🚀"
+    return """
+    <html>
+        <head>
+            <title>My Flask Website</title>
+        </head>
+        <body style="text-align:center; font-family:Arial;">
+            <h1>🚀 Welcome to My Flask App</h1>
+            <p>Successfully deployed on Railway</p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run()
